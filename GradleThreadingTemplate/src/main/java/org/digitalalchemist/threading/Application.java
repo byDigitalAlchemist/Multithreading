f8e969f74ac97a4033a45efb719af8e284e0cb1d
anwaar <developer.anwar@gmail.com>
2015-02-06 18:34:53 +0500

    At last First Gradle Project (OMG)

---
+++ GradleThreadingTemplate/src/main/java/org/digitalalchemist/threading/Application.java
@@ -0,0 +1,16 @@
+package org.digitalalchemist.threading;
+
+import org.joda.time.LocalTime;
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println(" Hello Gradle Threading Template");
+		LocalTime currentTime = new LocalTime();
+	    System.out.println("The current local time is: " + currentTime);
+		
+		
+	}
+
+}
